9e4d5716d88da27f224526ec7ce36070d3b06983
Rigina-18 <122827255+Rigina-18@users.noreply.github.com>
2023-12-02 19:15:23 +0300

    Update acid_test.py

--- acid_test.py
+++ acid_test.py
@@ -1,1 +1,14 @@
-# 123456
+# import os
+# import django
+# from django.db import transaction
+#
+#
+# Указать путь к файлу settings.py в переменной окружения
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "observer.settings")
+# django.setup()
+#
+#
+# from blog.models import Review, Like
+#
+# review = Review.objects.create(topic_id=1, title='BMV',)
+# like = Like.objects.create(user_id=1, review_id=3)
